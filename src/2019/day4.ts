d03e832207ffb4f5b81197b9d471d305c5fa3f20
CoreyShupe <coreyshupe101@gmail.com>
2020-10-30 02:31:40 -0400

    Finish day 4 solutions.

--- src/2019/day4.ts
+++ src/2019/day4.ts
@@ -32,8 +32,47 @@ function check(password: number): boolean {
     return flag;
 }
 
-function part2(input: string) {
-    
+function part2(input: string): number {
+    let split = input.split("-");
+    let range = {x: parseInt(split[0]), y: parseInt(split[1])};
+    let count = 0;
+    for (let x = range.x; x < range.y + 1; x++) {
+        if (check2(x)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function check2(password: number): boolean {
+    let passString = password.toString();
+    let flag2 = false;
+    let flag = false;
+    let past_value = undefined;
+
+    for (let i = 0; i < passString.length; i++) {
+        let x = passString[i];
+        if (past_value === undefined) {
+            past_value = x;
+            continue;
+        }
+        if (past_value > x) {
+            return false;
+        }
+        if (past_value === x) {
+            if (!flag2) {
+                if (i !== passString.length - 1 && passString[i + 1] === x) {
+                    flag2 = true;
+                } else {
+                    flag = true;
+                }
+            }
+        } else {
+            flag2 = false;
+        }
+        past_value = x;
+    }
+    return flag;
 }
 
 export function solution() {
